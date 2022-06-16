cc49650b4e2a6d879c55c40ee815bf607a0ff909
VadimKhudoleev <boby_toronto@mail.ru>
2022-06-16 23:26:12 +0300

    commit_1

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <div>
+            <a href='#s'>home</a>
+            <a href='#s'>news</a>
+            <a href='#s'>Messages</a>
+        </div>);
+}
+
+export  Header;
