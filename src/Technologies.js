fdc9c67b07d1a229020a26c9dc426027e437b7d6
VadimKhudoleev <boby_toronto@mail.ru>
2022-06-17 00:05:23 +0300

    commit 2

--- src/Technologies.js
+++ src/Technologies.js
@@ -3,13 +3,14 @@ const Technologies = () => {
         <div>
             <ul>
                 <p>Lorem5<p/>
-                <li>css</li>
-                <li>html</li>
-                   <li>js</li>
-                <li>react</li>
+                    <li>css</li>
+                    <li>html</li>
+                    <li>js</li>
+                    <li>react</li>
+                    <a>href=#</a>
             </ul>
         </div>
-    );
+);
 
 }
 
